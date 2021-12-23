7af77d8e433027db7d1b73ec6a149d807b7bb8c0
Joachim Kret <joachimkret@macbook.pro>
2021-12-23 19:44:11 +0100

    Added base project.

---
+++ Mocking/Sources/Mocking.h
@@ -0,0 +1,18 @@
+//
+//  Mocking.h
+//  Mocking
+//
+//  Created by Joachim Kret on 23/12/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Mocking.
+FOUNDATION_EXPORT double MockingVersionNumber;
+
+//! Project version string for Mocking.
+FOUNDATION_EXPORT const unsigned char MockingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Mocking/PublicHeader.h>
+
+
